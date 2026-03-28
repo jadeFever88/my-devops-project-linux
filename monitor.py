2d661453c7753725d9c710dcdfdf2513bbe8165a
jadeFever88 <d.dvolitko@gmail.com>
2026-03-28 18:00:57 +0200

    automatic container recovery via Python script

--- monitor.py
+++ monitor.py
@@ -1,20 +1,24 @@
 import requests
 import time
+import os
 
-# URL твого сайту (порт 8081 ми вказали в deploy.sh)
 URL = "http://localhost:8081"
+CONTAINER_NAME = "my-web-app"
 
-def check_site():
+def check_and_fix():
     try:
         response = requests.get(URL, timeout=3)
         if response.status_code == 200:
-            print(f"✅ [{time.strftime('%H:%M:%S')}] Все супер! Сайт відповідає.")
+            print(f"✅ [{time.strftime('%H:%M:%S')}] Сайт працює.")
         else:
-            print(f"⚠️ [{time.strftime('%H:%M:%S')}] Упс! Код помилки: {response.status_code}")
+            print(f"⚠️ [{time.strftime('%H:%M:%S')}] Код {response.status_code}. Спроба перезапуску...")
+            os.system(f"docker start {CONTAINER_NAME}")
     except requests.exceptions.ConnectionError:
-        print(f"🚨 [{time.strftime('%H:%M:%S')}] КАТАСТРОФА! Сайт не працює. Перевір Docker!")
+        print(f"🚨 [{time.strftime('%H:%M:%S')}] Сайт ПАВ! Оживляю контейнер...")
+        # Ось тут магія: Python сам пише команду в консоль за тебе
+        os.system(f"docker start {CONTAINER_NAME}")
 
-print("🕵️ Моніторинг запущено... (Ctrl+C щоб вийти)")
+print("👨‍⚕️ Лікар-моніторинг запущено...")
 while True:
-    check_site()
+    check_and_fix()
     time.sleep(5)
